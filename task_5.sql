62262ff9d579717296ce585c5dbdf04da242f0a6
Selamawit Shimeles <selamshimeles9@gmail.com>
2025-10-25 20:30:33 +0300

    update the code

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE ALX_BOOK_STORE;
 
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 USE ALX_BOOK_STORE;
 
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
 VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
